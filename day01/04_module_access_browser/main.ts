5fee546bb8748376f530ec8285256d7724589c7d
Deepak Rajamohan <thangarajamohan@Deepaks-MacBook-Pro.local>
2022-12-19 18:37:46 -0800

    04 module access

---
+++ day01/04_module_access_browser/main.ts
@@ -0,0 +1,20 @@
+/// <reference types="jquery" />
+
+interface Task {
+    to_string(): string
+}
+
+let task = {
+    to_string: function() { 
+        return "Hello World !!!";
+    }
+}
+
+function execute(task: Task) {
+    $("#description").html( "<B>" + task.to_string() + "</B>");
+    $("#description").css({ 'color': 'red', 'font-size': '150%' });
+}
+
+export function setColor () {
+    execute(task);
+}
