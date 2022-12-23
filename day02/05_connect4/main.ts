66b7de2ae00c3cbda580b77070cd8ce645faf115
Deepak Rajamohan <thangarajamohan@Deepaks-MacBook-Pro.local>
2022-12-23 09:12:32 -0800

    generate columns per row

--- day02/05_connect4/main.ts
+++ day02/05_connect4/main.ts
@@ -3,6 +3,22 @@ import { UserActionMessage } from './models/types'
 
 const webSocket = new WebSocket(location.origin.replace(/^http/, 'ws'))
 
+function generateRows(row) {
+    let row_innerhtml = ""
+
+    for (let col = 0; col < 7; ++col) {
+        let html_content = "<div class=\"letter\" ><div class=\"empty_position\" id=\"empty_position\" "
+        html_content +=  ` row="${row}" col="${col}"></div></div>`
+        row_innerhtml += html_content
+    }
+    return row_innerhtml
+}
+
+for (let row = 0; row < 7; ++row) {
+    document.querySelector(
+        `[coin-row-id='${row}']` ).innerHTML = generateRows(row)
+}
+
 $(".click_position").each((_, click_position) => {
     const row_clicked : number = parseInt(click_position.attributes.getNamedItem('row').value)
     click_position.addEventListener('click', (ev : MouseEvent) => {
