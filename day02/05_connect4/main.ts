58e8f5d9a90222d102c311604747c81935427ad6
Deepak Rajamohan <thangarajamohan@Deepaks-MacBook-Pro.local>
2022-12-23 08:59:38 -0800

    Publish user message

--- day02/05_connect4/main.ts
+++ day02/05_connect4/main.ts
@@ -1,6 +1,7 @@
-import 'https://code.jquery.com/jquery-3.6.0.min.js';
+import 'https://code.jquery.com/jquery-3.6.0.min.js'
+import { UserActionMessage } from './models/types'
 
-const webSocket = new WebSocket(location.origin.replace(/^http/, 'ws'));
+const webSocket = new WebSocket(location.origin.replace(/^http/, 'ws'))
 
 $(".click_position").each((_, click_position) => {
     const row_clicked : number = parseInt(click_position.attributes.getNamedItem('row').value)
@@ -11,3 +12,16 @@ $(".click_position").each((_, click_position) => {
         }))
     })
 })
+
+webSocket.onmessage = (event : MessageEvent) => {
+    const data : UserActionMessage = JSON.parse(event.data)
+    const row_clicked = data.position
+
+    $('div[id=empty_position]').each((_, empty_position) => {
+        const row = parseInt(empty_position.attributes.getNamedItem("row").value)
+        const col = parseInt(empty_position.attributes.getNamedItem("col").value)
+        if ( row === row_clicked && col === 0 ) {
+            empty_position.setAttribute('class', 'RED_COIN');
+        }
+    })
+}
